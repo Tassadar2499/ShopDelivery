8df59967daa76fc664a2eb87d3b8ace4674a3d52
Tassadar2499 <markevich.roma@gmail.com>
2021-01-28 00:04:27 +0500

    products bucket js

--- ShopDeliveryApplication/scripts/Bucket.ts
+++ ShopDeliveryApplication/scripts/Bucket.ts
@@ -3,7 +3,7 @@
 
 	public static addProductId(id: number) {
 		
-		let bucket = localStorage.getItem(Bucket.BucketKey);
+		const bucket = localStorage.getItem(Bucket.BucketKey);
 		let idArr: number[];
 		if (bucket == null) {
 			idArr = [id];
@@ -14,5 +14,64 @@
 		}
 
 		localStorage.setItem(Bucket.BucketKey, JSON.stringify(idArr));
+		Bucket.updateElementById(idArr, id);
+	}
+
+	public static removeProductId(id: number) {
+		const bucket = localStorage.getItem(Bucket.BucketKey);
+		if (bucket == null)
+			return;
+
+		let idArr: number[] = JSON.parse(bucket);
+		const indexOfNumber = idArr.indexOf(id);
+		idArr.splice(indexOfNumber, 1);
+
+		localStorage.setItem(Bucket.BucketKey, JSON.stringify(idArr));
+		Bucket.updateElementById(idArr, id);
+	}
+
+	public static productsPageLoad() {
+		const elements = Bucket.getProductsCountElements();
+		for (let i = 0; i < elements.length; i++) {
+			const element = elements[i];
+			const id: number = Bucket.getNumberFromElement(element);
+			element.textContent = 'Количество: ' + Bucket.getCountOfProducts(id);
+		}
+	}
+
+	private static updateElementById(idArr: number[], id: number) {
+		const element = Bucket.findElementByNumber(id);
+		element.textContent = 'Количество: ' + Bucket.getCountOfId(idArr, id);
+	}
+
+	private static findElementByNumber(num: number): Element {
+		const elements = Bucket.getProductsCountElements();
+		for (let i = 0; i < elements.length; i++) {
+			const element = elements[i];
+			if (num === Bucket.getNumberFromElement(element))
+				return element;
+		}
+	}
+
+	private static getProductsCountElements(): HTMLCollectionOf<Element> {
+		return document.getElementsByClassName('product-count');
+	}
+
+	private static getNumberFromElement(element: Element): number {
+		return parseInt(element.getAttribute('number'))
+	}
+
+	private static getCountOfProducts(id: number): number {
+		const bucket = localStorage.getItem(Bucket.BucketKey);
+		if (bucket == null)
+			return 0;
+
+		const idArr: number[] = JSON.parse(bucket);
+
+		return Bucket.getCountOfId(idArr, id);
+	}
+
+	private static getCountOfId(idArr: number[], id: number): number {
+		return idArr.filter(i => i == id).length;
 	}
 }
--- ShopDeliveryApplication/wwwroot/js/Bucket.js
+++ ShopDeliveryApplication/wwwroot/js/Bucket.js
@@ -12,8 +12,55 @@ var Bucket = /** @class */ (function () {
             idArr.push(id);
         }
         localStorage.setItem(Bucket.BucketKey, JSON.stringify(idArr));
+        Bucket.updateElementById(idArr, id);
+    };
+    Bucket.removeProductId = function (id) {
+        var bucket = localStorage.getItem(Bucket.BucketKey);
+        if (bucket == null)
+            return;
+        var idArr = JSON.parse(bucket);
+        var indexOfNumber = idArr.indexOf(id);
+        idArr.splice(indexOfNumber, 1);
+        localStorage.setItem(Bucket.BucketKey, JSON.stringify(idArr));
+        Bucket.updateElementById(idArr, id);
+    };
+    Bucket.productsPageLoad = function () {
+        var elements = Bucket.getProductsCountElements();
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var id = Bucket.getNumberFromElement(element);
+            element.textContent = 'Количество: ' + Bucket.getCountOfProducts(id);
+        }
+    };
+    Bucket.updateElementById = function (idArr, id) {
+        var element = Bucket.findElementByNumber(id);
+        element.textContent = 'Количество: ' + Bucket.getCountOfId(idArr, id);
+    };
+    Bucket.findElementByNumber = function (num) {
+        var elements = Bucket.getProductsCountElements();
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            if (num === Bucket.getNumberFromElement(element))
+                return element;
+        }
+    };
+    Bucket.getProductsCountElements = function () {
+        return document.getElementsByClassName('product-count');
+    };
+    Bucket.getNumberFromElement = function (element) {
+        return parseInt(element.getAttribute('number'));
+    };
+    Bucket.getCountOfProducts = function (id) {
+        var bucket = localStorage.getItem(Bucket.BucketKey);
+        if (bucket == null)
+            return 0;
+        var idArr = JSON.parse(bucket);
+        return Bucket.getCountOfId(idArr, id);
+    };
+    Bucket.getCountOfId = function (idArr, id) {
+        return idArr.filter(function (i) { return i == id; }).length;
     };
     Bucket.BucketKey = "bucket";
     return Bucket;
 }());
-//# sourceMappingURL=Bucket.js.map
+//# sourceMappingURL=bucket.js.map
