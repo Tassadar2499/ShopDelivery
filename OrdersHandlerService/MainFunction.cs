0a6c055f2a4fab81be47c2d556a9820750854a25
Tassadar2499 <markevich.roma@gmail.com>
2021-02-05 23:44:32 +0500

    azure function fixed

--- OrdersHandlerService/MainFunction.cs
+++ OrdersHandlerService/MainFunction.cs
@@ -8,7 +8,7 @@ namespace OrdersHandlerService
     public static class MainFunction
     {
         [FunctionName("StartFunction")]
-        public static void Run([QueueTrigger("product-orders-queue", Connection = "DefaultEndpointsProtocol=https;AccountName=productordersacc;AccountKey=ubmfUgb8XcXYWZNIWHbqJHPnzeZfCWpyjy5Bfc/tO8daCcmpCMlyqxDDrJ5A2wHHkPoYO+Lh+n3i6vaTZuyBsw==;EndpointSuffix=core.windows.net")]string myQueueItem, ILogger log)
+        public static void Run([QueueTrigger("product-orders-queue")] string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
