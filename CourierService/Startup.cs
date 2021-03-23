8020352b155f813325409aa61b2d4e98fb2f8eab
Tassadar2499 <markevich.roma@gmail.com>
2021-03-23 23:17:45 +0500

    added configurations

--- CourierService/Startup.cs
+++ CourierService/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ShopsDbEntities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,21 +17,22 @@ namespace CourierService
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
 
-		public IConfiguration Configuration { get; }
-
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			string connection = Configuration.GetConnectionString("DefaultConnection");
+			services.AddDbContext<MainDbContext>(options => options.UseSqlServer(connection));
+
 			services.AddControllers();
 			services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }));
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
