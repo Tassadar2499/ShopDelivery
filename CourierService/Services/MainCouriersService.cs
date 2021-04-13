ce11bee251cfbd58631881dcb55e11883e6670db
Tassadar2499 <markevich.roma@gmail.com>
2021-04-13 23:33:43 +0500

    test grpc + redis

--- CourierService/Services/MainCouriersService.cs
+++ CourierService/Services/MainCouriersService.cs
@@ -2,18 +2,32 @@ using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
 
 namespace CourierService
 {
 	public class MainCouriersService : MainCouriers.MainCouriersBase
 	{
 		private readonly ILogger<MainCouriersService> _logger;
-		public MainCouriersService(ILogger<MainCouriersService> logger) => _logger = logger;
+		private readonly ConnectionMultiplexer _redisConnection;
+		public MainCouriersService(ILogger<MainCouriersService> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+			var cacheConnection = configuration["CacheConnection"];
+			_redisConnection = ConnectionMultiplexer.Connect(cacheConnection);
+		}
 
 		public override Task<ActiveCourierReply> FindActiveCourier(ActiveCourierRequest request, ServerCallContext context)
 		{
 			var coords = (request.Longitude, request.Latitude);
 			_logger.LogDebug($"Recieved {coords}");
+
+			var database = _redisConnection.GetDatabase();
+			database.StringSet("test", "pepe");
+
+			Console.WriteLine(database.StringGet("test"));
+
 
 			throw new NotImplementedException();
 		}
---
+++ ProductsEntities/Entities/Courier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopsDbEntities.Entities
+{
+	public enum CourierStatus : byte
+	{ 
+		Active = 1,
+		Sleep = 2
+	}
+
+	public class Courier
+	{
+		public long Id { get; set; }
+		public CourierStatus Status { get; set; }
+		public string Host { get; set; }
+		public string Login { get; set; }
+		public string Password { get; set; }
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+	}
+}
