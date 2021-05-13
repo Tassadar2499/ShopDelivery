1ef440860cabb994853d0b47f4fc32890fdb6b5e
Tassadar2499 <markevich.roma@gmail.com>
2021-05-14 01:07:13 +0500

    added docker files

--- ProductsEntities/Entities/ProductEntities/Product.cs
+++ ProductsEntities/Entities/ProductEntities/Product.cs
@@ -3,31 +3,15 @@ using System;
 
 namespace ShopsDbEntities
 {
-	[JsonObject]
 	public class Product
 	{
-		[JsonProperty]
 		public long Id { get; set; }
-
-		[JsonProperty]
 		public ShopType ShopType { get; set; }
-
-		[JsonProperty]
 		public ProductCategory Category { get; set; }
-
-		[JsonProperty]
 		public ProductSubCategory SubCategory { get; set; }
-
-		[JsonProperty]
 		public string Name { get; set; }
-
-		[JsonProperty]
 		public float Price { get; set; }
-
-		[JsonProperty]
 		public float? Mass { get; set; }
-
-		[JsonProperty]
 		public string ImageUrl { get; set; }
 	}
 }
